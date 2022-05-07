c43124b50a10fe44e2d265eb4163a11357de0f45
Вячеслав <zve4202@yandex.ru>
2022-05-07 08:52:53 +0300

    final my-app

--- REACT/my-app/src/components/counter.jsx
+++ REACT/my-app/src/components/counter.jsx
@@ -1,14 +1,79 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () => {
-    const count = 0;
+    const [count, setCount] = useState(0);
+    const [tags, setTags] =useState(["tag1","tag2","tag3",]);
+
     const formatCount = () => {
         return count === 0 ? "empty" : count;
     }
+
+    const getBageClasses = () => {
+        let classes = "badge m-2 ";
+        classes += count === 0 ? "bg-warning" : "bg-primary";
+        return classes;
+    }
+    
+    const handleIncrement = () => {
+        setCount((prevState) => prevState + 1);
+    }
+    const handleDecrement = () => {
+        setCount((prevState) => {
+            if (prevState === 0)
+                return 0;
+             return prevState - 1;
+        });
+    }
+
+    const handleTagChange = (id) => {
+        setTags(prevState => prevState.filter(tag => tag !== id));
+    }
+
+    const renderTags = () => {
+        return tags.length !== 0 && tags.map((tag)=>(
+                    <li 
+                        key={tag} 
+                        className ="btn btn-primary btn-sm m-2"
+                        onClick={() => handleTagChange(tag)}
+                    >
+                        {tag}
+                    </li>
+                ));        
+        /*
+                return tags.length !== 0 ? tags.map((tag)=>(
+                    <li 
+                        key={tag} 
+                        className ="btn btn-primary btn-sm m-2"
+                        onClick={() => handleTagChange(tag)}
+                    >
+                        {tag}
+                    </li>
+                )): <span className ="badge bg-primary m-2">No tags</span>;
+        */
+    }
+
+    if (tags.length !== 0) {
+        return <ul>{renderTags()}</ul>
+    }
+
     return (
         <>
-            <span className="badge bg-primary m-2">{formatCount()}</span>
-            <button className="btn btn-primary btn-sm m-2">+</button>
+            <ul>
+                {renderTags()}
+            </ul>
+            <span className={getBageClasses()}>{formatCount()}</span>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={handleIncrement}
+            >
+                +
+            </button>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={handleDecrement}
+            >
+                -
+            </button>
         </>);
 }
 

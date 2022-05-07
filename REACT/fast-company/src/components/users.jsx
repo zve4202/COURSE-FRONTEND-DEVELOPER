d5827beb4717500ea71510e4da8a191891fa2a0b
Вячеслав <zve4202@yandex.ru>
2022-05-07 15:24:22 +0300

    fast-company task-1

--- REACT/fast-company/src/components/users.jsx
+++ REACT/fast-company/src/components/users.jsx
@@ -9,29 +9,43 @@ const Users = () => {
         setUsers(users.filter((user)=> user._id !== userId));
     }
     
-    const sklonenie = (number, txt) => {
+    const sklon = (number, txt) => {
         const cases = [2, 0, 1, 1, 1, 2];
         return txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
     }
 
-    const handlePhrase = (number) => {
-        
-        const clasess = (number === 0 ? "badge bg-danger" : "badge bg-primary") + " badge-sm p-2";
+    const handlePhrase = (number) => {         
+        const bg_name = number === 0 ? "danger" : "primary";
+        const class_name = `badge bg-${bg_name} fs-5`;
+        const endWord = sklon(number, ["к", "ка", "к"]);
         return (
-            <span className={clasess}>
-                {number === 0 ? "Никто с тобой не тусанёт" : `${number} челове${sklonenie(number, ["к", "ка", "к"])} тусуется с тобой сегодня`}
+            <span className={class_name}>
+                {number === 0 ? "Никто с тобой не тусанёт" : `${number} челове${endWord} тусуется с тобой сегодня`}
             </span> 
         );
+    }
+
+    const getQualities = (qualities) => {
+        return qualities.map(quality => {
+            const class_name = `badge bg-${quality.color} m-1`;
+            return (
+                <span 
+                    key={quality._id}
+                    className={class_name}>
+                    {quality.name}
+                </span>    
+            );
+        })
     }
 
     const handleUserRow = (user) => {
         return (
             <tr key={user._id}>
                 <td>{user.name}</td>
-                <td></td>
+                <td>{getQualities(user.qualities)}</td>
                 <td>{user.profession.name}</td>
                 <td>{user.completedMeetings}</td>
-                <td>{user.rate}</td>
+                <td>{user.rate}/5</td>
                 <td>
                     <button 
                         className="btn btn-danger btn-sm"

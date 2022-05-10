ecb5608a0f3523f5cfd1d29ee6f6dae46a9a24ef
Вячеслав <zve4202@yandex.ru>
2022-05-10 08:49:07 +0300

    КП - задание 2

---
+++ REACT/fast-company/src/components/bookmark.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Bookmark = ({_id, bookmark, onToggleBookMark}) => {
+    const iconClass = () =>{
+        return "bi " + (bookmark===true ? "bi-suit-heart-fill" : "bi-suit-heart"); 
+    }
+    const handleToggleBookMark =() => {
+        onToggleBookMark(_id);
+    }
+    return (
+        <i 
+            className={iconClass()}
+            onClick={handleToggleBookMark}
+        >               
+        </i>
+    );
+}
+
+export default Bookmark;

a3f8e072a4728a94970d80a682dbd7679e4c5030
Вячеслав <zve4202@yandex.ru>
2022-05-12 09:04:55 +0300

    5. Фильтрация

--- REACT/fast-company/src/app/components/users.jsx
+++ REACT/fast-company/src/app/components/users.jsx
@@ -3,6 +3,7 @@ import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
 import PropTypes from "prop-types";
+import GroupList from "./gropList";
 
 const Users = ({ users, ...rest }) => {
   const count = users.length;
@@ -17,6 +18,7 @@ const Users = ({ users, ...rest }) => {
 
   return (
     <>
+      <GroupList />
       {count > 0 && (
         <table className="table">
           <thead>

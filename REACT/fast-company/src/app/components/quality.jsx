2cc9158bed2cba9924c6a1a0001c9f322aff4c01
Вячеслав <zve4202@yandex.ru>
2022-05-14 09:38:57 +0300

    8. [КП] Реализация качества пользователей

--- REACT/fast-company/src/app/components/quality.jsx
+++ REACT/fast-company/src/app/components/quality.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Qualitie = ({ color, name }) => {
+const Quality = ({ color, name }) => {
   return <span className={"badge m-1 bg-" + color}>{name}</span>;
 };
-Qualitie.propTypes = {
+Quality.propTypes = {
   color: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired
 };
-export default Qualitie;
+export default Quality;
--- REACT/fast-company/src/app/components/userTable.jsx
+++ REACT/fast-company/src/app/components/userTable.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 import BookMark from "./bookmark";
+import QualitiesList from "./qualitiesList";
 
 const UserTable = ({
   users,
@@ -15,7 +16,10 @@ const UserTable = ({
 }) => {
   const columns = {
     name: { path: "name", name: "Имя" },
-    qualities: { name: "Качества" },
+    qualities: {
+      name: "Качества",
+      component: (user) => <QualitiesList qualities={user.qualities} />
+    },
     profession: { path: "profession.name", name: "Провфессия" },
     completedMeetings: { path: "completedMeetings", name: "Встретился, раз" },
     rate: { path: "rate", name: "Оценка" },

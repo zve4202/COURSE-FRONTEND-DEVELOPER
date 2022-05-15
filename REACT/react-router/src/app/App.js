0d43c8cca58515424c7ea115ceec1a35791acf66
Вячеслав <zve4202@yandex.ru>
2022-05-15 08:24:31 +0300

    Switch и его особенности

--- REACT/react-router/src/app/App.js
+++ REACT/react-router/src/app/App.js
@@ -1,19 +1,23 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard";
 import Home from "./components/home";
 import Login from "./components/login";
 import NavBar from "./components/navBar";
 import Posts from "./components/posts";
+import Stats from "./components/stats";
 
 function App() {
   return (
     <div>
       <NavBar />
       <h1>App</h1>
-      <Route path="/login" component={Login} />
-      <Route path="/posts" component={Posts} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/" component={Home} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/dashboard/stats" component={Stats} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
     </div>
   );
 }

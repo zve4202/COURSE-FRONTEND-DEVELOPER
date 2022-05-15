01633de0b84736ea7f22949b445ae2d0d683926d
Вячеслав <zve4202@yandex.ru>
2022-05-15 11:47:50 +0300

    9. Query параметры

--- REACT/react-router/src/app/components/posts.jsx
+++ REACT/react-router/src/app/components/posts.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import PostsList from "./postsList";
 import Post from "./post";
+import query from "query-string";
+import _ from "lodash";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
   const posts = [
     { id: 1, label: "Post 1" },
     { id: 2, label: "Post 2" },
     { id: 3, label: "Post 3" },
     { id: 4, label: "Post 4" },
   ];
+  console.log(location);
+  const search = query.parse(location.search);
   const postId = match.params.postId;
+  console.log(search);
+  const cropPost = search
+    ? _(posts).splice(0).take(search.count).value()
+    : posts;
   return (
     <>
       {postId ? (
         <Post id={postId} posts={posts} />
       ) : (
-        <PostsList posts={posts} />
+        <PostsList posts={cropPost} />
       )}
     </>
   );

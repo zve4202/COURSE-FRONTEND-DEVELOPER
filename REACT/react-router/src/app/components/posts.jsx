38bd9f3d151c9cbc9128bf94f90bb8a59b0e8005
Вячеслав <zve4202@yandex.ru>
2022-05-15 14:53:00 +0300

    11. History. Программная навигация

--- REACT/react-router/src/app/components/posts.jsx
+++ REACT/react-router/src/app/components/posts.jsx
@@ -1,29 +1,21 @@
 import React from "react";
 import PostsList from "./postsList";
 import Post from "./post";
-import query from "query-string";
-import _ from "lodash";
 
-const Posts = ({ match, location }) => {
+const Posts = ({ match, history }) => {
   const posts = [
     { id: 1, label: "Post 1" },
     { id: 2, label: "Post 2" },
     { id: 3, label: "Post 3" },
     { id: 4, label: "Post 4" },
   ];
-  console.log(location);
-  const search = query.parse(location.search);
   const postId = match.params.postId;
-  console.log(search);
-  const cropPost = search
-    ? _(posts).splice(0).take(search.count).value()
-    : posts;
   return (
     <>
       {postId ? (
-        <Post id={postId} posts={posts} />
+        <Post id={postId} posts={posts} history={history} />
       ) : (
-        <PostsList posts={cropPost} />
+        <PostsList posts={posts} />
       )}
     </>
   );

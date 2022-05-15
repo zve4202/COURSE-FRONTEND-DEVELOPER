3d9d84c65316a47dc1a919080966815031f13e3d
Вячеслав <zve4202@yandex.ru>
2022-05-15 15:24:21 +0300

    13. Вложенные маршруты

--- REACT/react-router/src/app/components/dashboard.jsx
+++ REACT/react-router/src/app/components/dashboard.jsx
@@ -1,5 +1,24 @@
+import { Link, Route, Switch } from "react-router-dom";
+import Edit from "./edit";
+import Stats from "./stats";
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/dashboard/edit">Edit</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/dashboard" component={Stats} />
+        <Route path="/dashboard/edit" component={Edit} />
+      </Switch>
+    </div>
+  );
 };
 
 export default Dashboard;

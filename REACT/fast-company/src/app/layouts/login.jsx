adaf685b331f3120c3a5c7c52239a9a984954bdc
Вячеслав <zve4202@yandex.ru>
2022-05-16 19:54:21 +0300

    9. Формы. Часть 1 -Отображение ошибки валидации

--- REACT/fast-company/src/app/layouts/login.jsx
+++ REACT/fast-company/src/app/layouts/login.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import TextField from "../components/textField";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({ ...prevState, [target.name]: target.value }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
+    };
+    const validate = () => {
+        const errors = {};
+        for (const fieldName in data) {
+            if (data[fieldName].trim() === "") {
+                errors[fieldName] = `${fieldName} обязательно для заполнения`;
+            }
+        }
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Электронная почта"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+                error={errors.email}
+            />
+            <TextField
+                label="Пароль"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                error={errors.password}
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default Login;

8a6bfeb2bdcb5ceae8d2fbb56e1ea984c6870671
Вячеслав <zve4202@yandex.ru>
2022-05-17 07:45:33 +0300

    9 -  Скрыть/Показать пароль

--- REACT/fast-company/src/app/components/textField.jsx
+++ REACT/fast-company/src/app/components/textField.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
+
     const getInputClasses = () => {
         return "form-control" + (error ? " is-invalid" : "");
     };
+
     return (
         <div className="mb-4">
             <label htmlFor={name} className="form-label">
                 {label}
             </label>
-            <input
-                type={type}
-                id={name}
-                name={name}
-                value={value}
-                onChange={onChange}
-                className={getInputClasses()}
-            />
-            {error && <div className="invalid-feedback">{error}</div>}
+            <div className="input-group has-validation">
+                <input
+                    type={showPassword ? "text" : type}
+                    id={name}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    className={getInputClasses()}
+                />
+                {type === "password" && (
+                    <button
+                        className="btn btn-outline-secondary"
+                        type="button"
+                        onClick={toggleShowPassword}
+                    >
+                        <i
+                            className={
+                                "bi bi-eye" + (showPassword ? "-slash" : "")
+                            }
+                        ></i>
+                    </button>
+                )}
+                {error && <div className="invalid-feedback">{error}</div>}
+            </div>
         </div>
     );
 };

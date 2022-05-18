810d3059259ec1f641b8de457ef115b9acdefd92
Вячеслав <zve4202@yandex.ru>
2022-05-18 08:55:25 +0300

    10 - Реструктуризация проекта

--- REACT/fast-company/src/app/App.js
+++ REACT/fast-company/src/app/App.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
-import NavBar from "./components/navBar";
+import NavBar from "./components/ui/navBar";
 
 function App() {
     return (

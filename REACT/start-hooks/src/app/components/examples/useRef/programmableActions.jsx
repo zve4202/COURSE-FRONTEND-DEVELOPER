1735288ca7f629258f522e8456abff8b40a3e290
Вячеслав <zve4202@yandex.ru>
2022-05-19 17:32:42 +0300

    11. Расширенные хуки и базовая оптимизация

---
+++ REACT/start-hooks/src/app/components/examples/useRef/programmableActions.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CardWrapper from "../../common/Card";
+import SmallTitle from "../../common/typografy/smallTitle";
+const ProgrammableActionsExample = () => {
+    return (
+        <CardWrapper>
+            <SmallTitle className="card-title">
+                Программируемые действия и свойства
+            </SmallTitle>
+        </CardWrapper>
+    );
+};
+
+export default ProgrammableActionsExample;
---
+++ REACT/start-hooks/src/app/components/examples/useRef/renderCount.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardWrapper from "../../common/Card";
+import SmallTitle from "../../common/typografy/smallTitle";
+const RenderCountExample = () => {
+    return (
+        <CardWrapper>
+            <SmallTitle>Подсчет количества рендеров</SmallTitle>
+        </CardWrapper>
+    );
+};
+
+export default RenderCountExample;

15db658367b3a7c811a08c2fc3ba8d54caba379b
Вячеслав <zve4202@yandex.ru>
2022-05-19 10:20:08 +0300

    Samples Init

---
+++ REACT/samples/components/loginFormWithYup.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import CheckBoxField from "../common/form/checkBoxField";
+import TextField from "../common/form/textField";
+import * as yup from "yup";
+
+const LoginForm = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    stayOn: false,
+  });
+  const [errors, setErrors] = useState({});
+  const handleChange = (target) => {
+    setData((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+  };
+  const validateScheme = yup.object().shape({
+    password: yup
+      .string()
+      .required("Пароль обязателен для заполнения")
+      .matches(
+        /^(?=.*[A-Z])/,
+        "Пароль должен содержать хотя бы одну заглавную букву"
+      )
+      .matches(
+        /^(?=.*[a-z])/,
+        "Пароль должен содержать хотя бы одну прописную букву"
+      )
+      .matches(/(?=.*[1-9])/, "Пароль должен содержать хотя бы одно число")
+      .matches(
+        /(?=.*[!@#$%^&*])/,
+        "Пароль должен содержать хотя бы один из специальных символов !@#$%^&*"
+      )
+      .matches(/(?=.{8,})/, "Пароль должен состоять минимум из 8 символов"),
+    email: yup
+      .string()
+      .required("Электронная почта обязательна для заполнения")
+      .email("Email введен некорректно"),
+  });
+
+  useEffect(() => {
+    validate();
+  }, [data]);
+  const validate = () => {
+    validateScheme
+      .validate(data)
+      .then(() => setErrors({}))
+      .catch((err) => setErrors({ [err.path]: err.message }));
+    return Object.keys(errors).length === 0;
+  };
+  const isValid = Object.keys(errors).length === 0;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Электронная почта"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+        error={errors.email}
+      />
+      <TextField
+        label="Пароль"
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+        error={errors.password}
+      />
+      <CheckBoxField value={data.stayOn} onChange={handleChange} name="stayOn">
+        Оставаться в системе
+      </CheckBoxField>
+      <button
+        className="btn btn-primary w-100 mx-auto"
+        type="submit"
+        disabled={!isValid}
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+LoginForm.propTypes = {};
+
+export default LoginForm;

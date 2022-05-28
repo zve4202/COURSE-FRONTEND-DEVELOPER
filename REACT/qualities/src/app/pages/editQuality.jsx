505af747a1378169d27e26621e9be9c579c60bf4
Вячеслав <zve4202@yandex.ru>
2022-05-28 11:34:05 +0300

    14 -  Ожидаемые и неожиданные ошибки

--- REACT/qualities/src/app/pages/editQuality.jsx
+++ REACT/qualities/src/app/pages/editQuality.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
+import axios from "axios";
 
 const EditQualityPage = () => {
-    return (
-        <>
-            <h1>Edit Quality Page</h1> <EditForm />
-        </>
-    );
+  const id = useParams().id;
+  const qualityEnPoint = `http://localhost:4000/api/v1/quality/${id}`;
+  const [quality, setQuality] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get(qualityEnPoint);
+      setQuality(data.content);
+    }
+    fetchData();
+  }, [qualityEnPoint]);
+
+  const handleSubmit = async (data) => {
+    try {
+      await axios
+        .put(qualityEnPoint, data)
+        .then((res) => console.log(res.data.content));
+    } catch (error) {
+      // ожидаемая ошибка
+      const expectedErrors =
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500;
+      if (!expectedErrors) {
+        console.log("unexpected error");
+      } else {
+        console.log("Expected error");
+      }
+    }
+  };
+
+  return (
+    <>
+      <h1>Edit Quality Page</h1>{" "}
+      {quality === null ? (
+        "Loading..."
+      ) : (
+        <EditForm data={quality} onSubmit={handleSubmit} />
+      )}
+    </>
+  );
 };
 
 export default EditQualityPage;
--- REACT/qualities/src/app/pages/qualititesList.jsx
+++ REACT/qualities/src/app/pages/qualititesList.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import QualitiesTable from "../components/ui/qualitiesTable";
+import axios from "axios";
 
 const QualitiesListPage = () => {
-    const history = useHistory();
-    const handleEdit = (param) => {
-        console.log(param);
-        history.push(`/edit/${param}`);
-    };
-    const handleDelete = (param) => {
-        console.log(param);
-    };
-    return (
-        <>
-            <h1>Qualitites List Page</h1>
-            <QualitiesTable
-                onDelete={handleDelete}
-                onEdit={handleEdit}
-                data={[]}
-            />
-        </>
-    );
+  const [qualities, setQualities] = useState([]);
+  const history = useHistory();
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get("http://localhost:4000/api/v1/quality");
+      setQualities(data.content);
+    }
+    fetchData();
+  }, []);
+  const handleEdit = (param) => {
+    console.log(param);
+    history.push(`/edit/${param}`);
+  };
+  const handleDelete = (param) => {
+    console.log(param);
+  };
+  return (
+    <>
+      <h1>Qualitites List Page</h1>
+      <QualitiesTable
+        onDelete={handleDelete}
+        onEdit={handleEdit}
+        data={qualities}
+      />
+    </>
+  );
 };
 
 export default QualitiesListPage;

c0b8d58e443cbb8f0eb5037103e8c3aab2dcea60
Вячеслав <zve4202@yandex.ru>
2022-05-31 11:14:57 +0300

    14 - final

--- REACT/qualities/src/app/hooks/useQualities.jsx
+++ REACT/qualities/src/app/hooks/useQualities.jsx
@@ -5,87 +5,88 @@ import qualityService from "../services/quality.service";
 const QualitiesContex = React.createContext();
 
 export const useQualities = () => {
-    return useContext(QualitiesContex);
+  return useContext(QualitiesContex);
 };
 
 export const QualitiesProvider = ({ children }) => {
-    const [qualities, setQualities] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoading, setLoading] = useState(true);
-    const prevState = useRef();
-    useEffect(() => {
-        const getQualities = async () => {
-            try {
-                const { content } = await qualityService.fetchAll();
-                setQualities(content);
-                setLoading(false);
-            } catch (error) {
-                errorCatcher(error);
-            }
-        };
-        getQualities();
-    }, []);
-    const getQuality = (id) => {
-        return qualities.find((q) => q._id === id);
+  const [qualities, setQualities] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const prevState = useRef();
+  useEffect(() => {
+    const getQualities = async () => {
+      try {
+        const { content } = await qualityService.fetchAll();
+        setQualities(content);
+        setLoading(false);
+      } catch (error) {
+        errorCatcher(error);
+      }
     };
-    const updateQuality = async ({ _id: id, ...data }) => {
-        try {
-            const { content } = await qualityService.update(id, data);
-            setQualities((prevState) =>
-                prevState.map((item) => {
-                    if (item._id === content._id) {
-                        return content;
-                    }
-                    return item;
-                })
-            );
+    getQualities();
+  }, []);
+  const getQuality = (id) => {
+    return qualities.find((q) => q._id === id);
+  };
+  const updateQuality = async ({ _id: id, ...data }) => {
+    try {
+      const { content } = await qualityService.update(id, data);
+      setQualities((prevState) =>
+        prevState.map((item) => {
+          if (item._id === content._id) {
             return content;
-        } catch (error) {
-            errorCatcher(error);
-        }
-    };
-    const addQuality = async (data) => {
-        try {
-            const { content } = await qualityService.create(data);
-            setQualities((prevState) => [...prevState, content]);
-            return content;
-        } catch (error) {
-            errorCatcher(error);
-        }
-    };
-    const deleteQuality = async (id) => {
-        prevState.current = qualities;
-        try {
-            const { content } = await qualityService.delete(id);
-            setQualities((prevState) => {
-                return prevState.filter((item) => item._id !== content._id);
-            });
-        } catch (error) {
-            errorCatcher(error);
-        }
-    };
-    function errorCatcher(error) {
-        const { message } = error.response.data;
-        setError(message);
+          }
+          return item;
+        })
+      );
+      return content;
+    } catch (error) {
+      errorCatcher(error);
     }
-    useEffect(() => {
-        if (error !== null) {
-            toast(error);
-            setError(null);
-        }
-    }, [error]);
+  };
+  const addQuality = async (data) => {
+    try {
+      const { content } = await qualityService.create(data);
+      setQualities((prevState) => [...prevState, content]);
+      return content;
+    } catch (error) {
+      errorCatcher(error);
+    }
+  };
+  const deleteQuality = async (id) => {
+    prevState.current = qualities;
+    try {
+      const { content } = await qualityService.delete(id);
+      setQualities((prevState) => {
+        return prevState.filter((item) => item._id !== content._id);
+      });
+    } catch (error) {
+      errorCatcher(error);
+    }
+  };
+  function errorCatcher(error) {
+    console.log(error);
+    const { message } = error.response.data || error;
+    setError(message);
+  }
+  useEffect(() => {
+    if (error !== null) {
+      toast.error(error);
+      setError(null);
+    }
+  }, [error]);
 
-    return (
-        <QualitiesContex.Provider
-            value={{
-                qualities,
-                getQuality,
-                updateQuality,
-                addQuality,
-                deleteQuality,
-            }}
-        >
-            {!isLoading ? children : <h1>Qualities Loading ....</h1>}
-        </QualitiesContex.Provider>
-    );
+  return (
+    <QualitiesContex.Provider
+      value={{
+        qualities,
+        getQuality,
+        updateQuality,
+        addQuality,
+        deleteQuality,
+      }}
+    >
+      {!isLoading ? children : <h1>Qualities Loading ....</h1>}
+    </QualitiesContex.Provider>
+  );
 };
--- REACT/qualities/src/app/services/quality.service.js
+++ REACT/qualities/src/app/services/quality.service.js
@@ -19,7 +19,7 @@ const qualityService = {
     return data;
   },
   delete: async (id) => {
-    const { data } = await httpService.delete(qualityEndpoint + id);
+    const { data } = await httpService.delete(qualityEndpoint + id + "11");
     return data;
   },
 };

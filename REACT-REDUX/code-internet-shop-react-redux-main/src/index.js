bc8fd4ce711d524c2ec96558ff698c0175b46881
Вячеслав <zve4202@yandex.ru>
2022-06-17 14:01:16 +0300

    Магазин на React-Redux. Урок 4

--- REACT-REDUX/code-internet-shop-react-redux-main/src/index.js
+++ REACT-REDUX/code-internet-shop-react-redux-main/src/index.js
@@ -1,3 +1,29 @@
-import './main.css'
+import "./main.css";
 
-document.write('Hello React/Redux!')
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {applyMiddleware, legacy_createStore} from "redux";
+import {createBrowserHistory} from "history";
+import thunk from "redux-thunk";
+import {ConnectedRouter, routerMiddleware} from "connected-react-router";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+import createRootReduser from "reducers";
+import Layout from "containers/layout";
+
+const history = createBrowserHistory();
+const middlewares = [thunk, routerMiddleware(history)];
+const store = legacy_createStore(
+    createRootReduser(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Layout />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById("root")
+);
---
+++ REACT-REDUX/code-internet-shop-react-redux-main/src/node_modules/api/mockPhones.js
@@ -0,0 +1,98 @@
+export default [
+  {
+    id: '1',
+    categoryId: '1',
+    name: 'Apple iPhone 5c',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies lorem odio, at laoreet tellus sodales in. Nullam maximus eros ut tortor ultricies rutrum. Aliquam euismod lacus non est egesta',
+    price: 823,
+    image: 'http://i.piccy.info/i9/df2c561f4cfd692648dda7aa1f1f1553/1578477094/10820/1354931/iphone5c_selection_hero_2013_4_240.jpg',
+    cpu: '1.3GHz Apple A6',
+    camera: '8mp (3264x2448)',
+    size: '124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)',
+    weight: '132 grams (4.7 ounces) with battery',
+    display: '4.0 326 pixel density',
+    battery: '1480 mAh',
+    memory: '16GB, 32GB and RAM 1 GB'
+  },
+  {
+    id: '2',
+    categoryId: '1',
+    name: 'Apple iPhone 6',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies lorem odio, at laoreet tellus sodales in. Nullam maximus eros ut tortor ultricies rutrum. Aliquam euismod lacus non est egesta',
+    price: 953,
+    image: 'http://i.piccy.info/i9/30f03dfa20bcab59bcfd6552fdfe80d6/1577706129/8376/1354931/51u6y9Rm8QL_SY300__240.jpg',
+    cpu: '1.3GHz Apple A6',
+    camera: '8mp (3264x2448)',
+    size: '124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)',
+    weight: '132 grams (4.7 ounces) with battery',
+    display: '4.0 326 pixel density',
+    battery: '1480 mAh',
+    memory: '16GB, 32GB and RAM 1 GB'
+  },
+  {
+    id: '3',
+    categoryId: '4',
+    name: 'Lenovo A6000',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies lorem odio, at laoreet tellus sodales in. Nullam maximus eros ut tortor ultricies rutrum. Aliquam euismod lacus non est egesta',
+    price: 764,
+    image: 'http://i.piccy.info/i9/9eea113e3a48e377ee646ef7cb42101f/1577707677/10654/1354931/_35_1__240.jpg',
+    cpu: '1.3GHz Apple A6',
+    camera: '8mp (3264x2448)',
+    size: '124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)',
+    weight: '132 grams (4.7 ounces) with battery',
+    display: '4.0 326 pixel density',
+    battery: '1480 mAh',
+    memory: '16GB, 32GB and RAM 1 GB'
+  },
+  {
+    id: '4',
+    categoryId: '5',
+    name: 'Nokia Lumia 1520',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies lorem odio, at laoreet tellus sodales in. Nullam maximus eros ut tortor ultricies rutrum. Aliquam euismod lacus non est egesta',
+    price: 674,
+    image: 'http://i.piccy.info/i9/19b2c5bdc5f1a2bb7470056a6ab586a5/1578477357/10124/1354931/Lumia1520_Front_Back_png_240.jpg',
+    cpu: '1.3GHz Apple A6',
+    camera: '8mp (3264x2448)',
+    size: '124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)',
+    weight: '132 grams (4.7 ounces) with battery',
+    display: '4.0 326 pixel density',
+    battery: '1480 mAh',
+    memory: '16GB, 32GB and RAM 1 GB'
+  },
+  {
+    id: '5',
+    categoryId: '3',
+    name: 'HTC One',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies lorem odio, at laoreet tellus sodales in. Nullam maximus eros ut tortor ultricies rutrum. Aliquam euismod lacus non est egesta',
+    price: 674,
+    image: 'http://i.piccy.info/i9/26a79ebd6ac476dab03b180a635c4b0d/1577707825/9787/1354931/htc_one_m7_802w_dual_sim_silver_240.jpg',
+    cpu: '1.3GHz Apple A6',
+    camera: '8mp (3264x2448)',
+    size: '124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)',
+    weight: '132 grams (4.7 ounces) with battery',
+    display: '4.0 326 pixel density',
+    battery: '1480 mAh',
+    memory: '16GB, 32GB and RAM 1 GB'
+  },
+  {
+    id: '6',
+    categoryId: '2',
+    name: 'Samsung Galaxy S6',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultricies lorem odio, at laoreet tellus sodales in. Nullam maximus eros ut tortor ultricies rutrum. Aliquam euismod lacus non est egesta',
+    price: 674,
+    image: 'http://i.piccy.info/i9/7232d75835af89f923e3750484949606/1577706988/7502/1354931/Agnes_Case_for_Samsung_Galaxy_S6_1_92643_thumb_240.jpg',
+    cpu: '1.3GHz Apple A6',
+    camera: '8mp (3264x2448)',
+    size: '124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)',
+    weight: '132 grams (4.7 ounces) with battery',
+    display: '4.0 326 pixel density',
+    battery: '1480 mAh',
+    memory: '16GB, 32GB and RAM 1 GB'
+  }
+]

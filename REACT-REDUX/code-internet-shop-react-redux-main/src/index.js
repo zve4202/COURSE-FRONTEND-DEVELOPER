527f6dd41b165b6fbc0ebd61238a34dd31c3f9b2
Вячеслав <zve4202@yandex.ru>
2022-06-19 09:51:20 +0300

    categories

--- REACT-REDUX/code-internet-shop-react-redux-main/src/index.js
+++ REACT-REDUX/code-internet-shop-react-redux-main/src/index.js
@@ -1,27 +1,27 @@
-import "./main.css";
+import './main.css'
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { applyMiddleware, legacy_createStore } from "redux";
-import { createBrowserHistory } from "history";
-import thunk from "redux-thunk";
-import { ConnectedRouter, routerMiddleware } from "connected-react-router";
-import { composeWithDevTools } from "redux-devtools-extension";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {applyMiddleware, legacy_createStore} from 'redux'
+import {createBrowserHistory} from 'history'
+import thunk from 'redux-thunk'
+import {ConnectedRouter, routerMiddleware} from 'connected-react-router'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-import createRootReduser from "reducers";
-import routes from "./modules/routes";
+import createRootReduser from 'reducers'
+import routes from 'routes'
 
-const history = createBrowserHistory();
-const middlewares = [thunk, routerMiddleware(history)];
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
 const store = legacy_createStore(
-    createRootReduser(history),
-    composeWithDevTools(applyMiddleware(...middlewares))
-);
+  createRootReduser(history),
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>{routes}</ConnectedRouter>
-    </Provider>,
-    document.getElementById("root")
-);
+  <Provider store={store}>
+    <ConnectedRouter history={history}>{routes}</ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)

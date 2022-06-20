cc5654179dbcb652cbaad7e8846dbc5e9c64f5d9
Вячеслав <zve4202@yandex.ru>
2022-06-20 13:57:43 +0300

    18 Разработка Redux с нуля. Reducer

--- REACT/react-rdux-app/src/index.js
+++ REACT/react-rdux-app/src/index.js
@@ -2,43 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 
-function creatrStore(initialState) {
+function taskReducer(state, action) {
+  switch (action.type) {
+    case "task/complited":
+      const newArray = [...state];
+      const index = newArray.findIndex((item) => item.id == action.payload.id);
+      newArray[index].completed = true;
+      return newArray;
+    default:
+      return state;
+  }
+}
+
+function creatrStore(reducer, initialState) {
   let state = initialState;
   function getState() {
     return state;
   }
-  function dispatch({ type, payload }) {
-    switch (type) {
-      case "task/complited":
-        const newArray = [...state];
-        const index = newArray.findIndex((item) => item.id == payload.id);
-        newArray[index].completed = true;
-        state = newArray;
-        console.log(state);
-        return state;
-
-      default:
-        return payload;
-    }
+  function dispatch(action) {
+    state = reducer(state, action);
   }
   return { getState, dispatch };
 }
 
-const store = creatrStore([{ id: 1, description: "Task 1", completed: false }]);
+const store = creatrStore(taskReducer, [
+  { id: 1, description: "Task 1", completed: false },
+]);
 const App = () => {
   console.log(store.getState());
-
-  // console.log(store.getState());
+  function handleClick() {
+    store.dispatch({ type: "task/complited", payload: { id: 1 } });
+    console.log(store.getState());
+  }
   return (
     <>
       <h1 className="btn btn-dark">
         App
-        <button
-          className="btn btn-danger"
-          onClick={() =>
-            store.dispatch({ type: "task/complited", payload: { id: 1 } })
-          }
-        >
+        <button className="btn btn-danger" onClick={handleClick}>
           Complete
         </button>
       </h1>

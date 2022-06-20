b8daa6297f7417797fcefa6bf0045a2625296bf8
Вячеслав <zve4202@yandex.ru>
2022-06-20 13:37:47 +0300

    18 Разработка Redux с нуля. Dispatch

--- REACT/react-rdux-app/src/index.js
+++ REACT/react-rdux-app/src/index.js
@@ -7,13 +7,43 @@ function creatrStore(initialState) {
   function getState() {
     return state;
   }
-  return { getState };
+  function dispatch({ type, payload }) {
+    switch (type) {
+      case "task/complited":
+        const newArray = [...state];
+        const index = newArray.findIndex((item) => item.id == payload.id);
+        newArray[index].completed = true;
+        state = newArray;
+        console.log(state);
+        return state;
+
+      default:
+        return payload;
+    }
+  }
+  return { getState, dispatch };
 }
 
-const store = creatrStore({ id: 1, description: "Task 1", completed: false });
+const store = creatrStore([{ id: 1, description: "Task 1", completed: false }]);
 const App = () => {
-  store.getState();
-  return <h1 className="btn btn-dark">App</h1>;
+  console.log(store.getState());
+
+  // console.log(store.getState());
+  return (
+    <>
+      <h1 className="btn btn-dark">
+        App
+        <button
+          className="btn btn-danger"
+          onClick={() =>
+            store.dispatch({ type: "task/complited", payload: { id: 1 } })
+          }
+        >
+          Complete
+        </button>
+      </h1>
+    </>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

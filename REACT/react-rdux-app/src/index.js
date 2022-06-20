ff95ec12d06b695bb6a85c9a42b9a8c97b188787
Вячеслав <zve4202@yandex.ru>
2022-06-20 12:18:29 +0300

    18 - Чистые функции (pure functions)

--- REACT/react-rdux-app/src/index.js
+++ REACT/react-rdux-app/src/index.js
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import { compose, pipe } from "lodash/fp";
 
 const App = () => {
-  const x = 2;
-  const double = (number) => number * 2;
-  const square = (number) => number * number;
-  const half = (number) => number / 2;
-  const divide = (num2) => (num1) => num1 / num2;
-
-  const mathCalc = pipe(double, square, half, divide(3));
-  return <h1 className="btn btn-dark">App {mathCalc(x)}</h1>;
+  return <h1 className="btn btn-dark">App</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

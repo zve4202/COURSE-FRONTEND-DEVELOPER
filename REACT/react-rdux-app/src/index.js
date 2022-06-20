72c5060359bf3b3dd4cc4ccf17dfe826079da1b4
Вячеслав <zve4202@yandex.ru>
2022-06-20 11:54:22 +0300

    18  Композиция функций

--- REACT/react-rdux-app/src/index.js
+++ REACT/react-rdux-app/src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
+import { compose, pipe } from "lodash/fp";
 
 const App = () => {
-  return <h1 className="btn btn-dark">App</h1>;
+  const x = 2;
+  const double = (number) => number * 2;
+  const square = (number) => number * number;
+  const half = (number) => number / 2;
+
+  // const mathCalc = compose(half, square, double);
+  const mathCalc = pipe(double, square, half);
+  return <h1 className="btn btn-dark">App {mathCalc(x)}</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

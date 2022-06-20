6deea71b52f74f51ffc2956adca5a1753450c793
Вячеслав <zve4202@yandex.ru>
2022-06-20 12:33:43 +0300

    18 Неизменяемый объект

--- REACT/react-rdux-app/src/index.js
+++ REACT/react-rdux-app/src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 
 const App = () => {
+  const [state, setState] = useState({});
+  const obj1 = { id: 2, name: "Name", author: { name: "A Name" } };
+  const obj2 = { ...obj1, author: { ...obj1.author } };
+  console.log(obj1.author === obj2.author);
   return <h1 className="btn btn-dark">App</h1>;
 };
 

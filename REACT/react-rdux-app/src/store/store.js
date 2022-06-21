5e251a4eea85fe3b49c50544583da62165752412
Вячеслав <zve4202@yandex.ru>
2022-06-21 09:42:43 +0300

    18 Создаём Middleware. Логгер

--- REACT/react-rdux-app/src/store/store.js
+++ REACT/react-rdux-app/src/store/store.js
@@ -1,8 +1,19 @@
-import { legacy_createStore } from "redux";
+import { legacy_createStore, compose, applyMiddleware } from "redux";
+
 import taskReducer from "./task";
+import logger from "./middleware/logger";
+
+const middlewareEnhancer = applyMiddleware(logger);
 
 function configureStore() {
-  return legacy_createStore(taskReducer);
+  return legacy_createStore(
+    taskReducer,
+    compose(
+      middlewareEnhancer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
 }
 
 export default configureStore;
